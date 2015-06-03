92f4c3c37334802704f50db065a17105295d2baf
James McCann <jmccnz@gmail.com>
2015-06-03 21:18:12 +1200

    Adding vim plugins

--- install.sh
+++ install.sh
@@ -6,3 +6,13 @@ ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/
 # install packages via homebrew and homebrew-cask
 cat brew/brew_install | xargs -n1 brew install
 cat brew/cask_install | xargs -n1 brew cask install
+
+# symlinks
+ln -sf ~/.dotfiles/zsh/.zshrc ~/.zshrc
+ln -sf ~/.dotfiles/git/.gitconfig ~/.gitconfig
+ln -sf ~/.dotfiles/git/.gitignore ~/.gitignore
+ln -sf ~/.dotfiles/vim/.vimrc.after ~/.vimrc.after
+ln -sf ~/.dotfiles/vim/.gvimrc.after ~/.gvimrc.after
+
+# install oh-my-zsh
+curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
