fb06268a344b2f8fe6daa9a4f1f707148a577d5c
James McCann <jmccnz@gmail.com>
2022-03-09 14:18:48 +0100

    install: update install ordering, update homebrew/cask, add keyboard remap

--- install.sh
+++ install.sh
@@ -1,25 +1,29 @@
 #!/bin/sh
 
 # install homebrew
-ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-
-# install brew / cask pre-reqs
-brew tap caskroom/cask
-brew cask install java
+# /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 # install packages via homebrew and homebrew-cask
-cat brew/brew_install | xargs -n1 brew install
-cat brew/cask_install | xargs -n1 brew cask install
-
-# symlinks
-ln -sf ~/.dotfiles/zsh/.zshrc ~/.zshrc
-ln -sf ~/.dotfiles/git/.gitconfig ~/.gitconfig
-ln -sf ~/.dotfiles/git/.gitignore ~/.gitignore
-ln -sf ~/.dotfiles/vim ~/.vim
-ln -sf ~/.dotfiles/.vimrc ~/.vimrc
+cat brew/brew_install | xargs -n1 /opt/homebrew/bin/brew install
+cat brew/cask_install | xargs -n1 /opt/homebrew/bin/brew install
 
 # install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+# symlinks
+ln -sf ~/dotfiles/zsh/.zshrc ~/.zshrc
+ln -sf ~/dotfiles/git/.gitconfig ~/.gitconfig
+ln -sf ~/dotfiles/git/.gitignore ~/.gitignore
+ln -sf ~/dotfiles/vim ~/.vim
+ln -sf ~/dotfiles/.vimrc ~/.vimrc
+
+# set fast key refresh
+defaults write -g InitialKeyRepeat -int 10
+defaults write -g KeyRepeat -int 1
+
+# uncomment to remap ~,§ on international keyboard
+# cp ./international-keyboard-remap.plist /Library/LaunchDaemons/org.custom.keyboard-remap.plist
+# sudo launchctl load -w /Library/LaunchDaemons/org.custom.keyboard-remap.plist
 
 # kill desktop icons
 defaults write com.apple.finder CreateDesktop false
--- zsh/.zshrc
+++ zsh/.zshrc
@@ -11,28 +11,23 @@ plugins=(
 [[ -s ~/.autojump/etc/profile.d/autojump.sh ]] && . ~/.autojump/etc/profile.d/autojump.sh
 source $ZSH/oh-my-zsh.sh
 
-PROMPT=$PROMPT'$(kube_ps1) '
-
 alias hist='history | grep'
 alias gdc='git diff --cached'
 alias be='bundle exec'
-alias t15='termdown 15m -s --no-figlet && afplay /System/Library/Sounds/Glass.aiff'
 alias hs='bundle exec hanami server'
 alias hc='bundle exec hanami console'
 alias uuid="uuidgen | tr -d - | tr -d '\n' | tr '[:upper:]' '[:lower:]'  | pbcopy && pbpaste && echo"
 
 export EDITOR=vim
-
-export PATH=/usr/local/bin:/usr/local/sbin:~/.npm/bin:/usr/local/share/npm/bin:/usr/bin:/bin:/usr/sbin:/sbin
 export GOPATH=$HOME/go
 export GOROOT=/usr/local/opt/go/libexec
-export PATH=$PATH:$GOPATH/bin:$GOROOT/bin
+export PATH=/opt/homebrew/bin:/usr/local/bin:/usr/local/sbin:~/.npm/bin:/usr/local/share/npm/bin:/usr/bin:/bin:/usr/sbin:/sbin:$GOPATH/bin:$GOROOT/bin
 export GO111MODULE=on
 
 if which rbenv > /dev/null; then eval "$(rbenv init -)"; fi
 function npm-do { (PATH=$(npm bin):$PATH; eval $@;) }
 
-export GPG_TTY="$(tty)"
-export SSH_AUTH_SOCK=$(gpgconf --list-dirs agent-ssh-socket)
-gpg-connect-agent updatestartuptty /bye
+# export GPG_TTY="$(tty)"
+# export SSH_AUTH_SOCK=$(gpgconf --list-dirs agent-ssh-socket)
+# gpg-connect-agent updatestartuptty /bye
 # gpgconf --launch gpg-agent
